4a45da32da15af536d903f7e9b2b50a07bae4dc9
zlzlzb <zlzlzb@nate.com>
2017-06-14 17:57:17 +0900

    day3 end

--- day3/router.js
+++ day3/router.js
@@ -1,8 +1,18 @@
 var http=require('http');
 var express=require('express');
 var bodyParser = require('body-parser');
-
+//var ejs = require('ejs');
+var fs = require('fs');
 var app = express();
+
+app.set("views",__dirname+"/template");
+app.set("view engine","ejs");
+
+app.get('/first.html',function(req,res){
+	
+	//res.sendFile(__dirname+'/public/index.html');
+	res.redirect('/next.html');
+});
 
 
 app.use(express.static(__dirname+"/public"));
@@ -12,19 +22,35 @@ app.get("/Login",function(req,res){
 	var id= req.query.id;
 	var pwd = req.query.pwd;
 	
-	res.send("<h1>GET</h1>"+"id : " + id + " pwd : " + pwd);
+//		fs.readFile(__dirname+"template/login.html",'utf8',function(err,data){
+//		var html = ejs.render(data,{id:id, pwd:pwd});
+//		res.send(html);
+//	
+//		});		
+		
+	res.render("login",{id:id,pwd:pwd});
+
+	
+	//res.send("<h1>GET</h1>"+"id : " + id + " pwd : " + pwd);
 });
 
 app.post("/Login",function(req,res){
 	var id= req.body.id;
 	var pwd = req.body.pwd;
-	res.send("<h1>POST</h1>"+"id : " + id + " pwd : " + pwd);
+	//res.send("<h1>POST</h1>"+"id : " + id + " pwd : " + pwd);
+	
+	res.render("login",{id:id,pwd:pwd});
 });
 
 app.get("/user/:id/:pwd",function(req,res){
 	var id = req.params['id'];
 	var pwd = req.params['pwd'];
 	res.send("id : " + id + " pwd : " + pwd);
+});
+
+app.get('/1st.html',function(req,res){
+	
+	res.sendFile(__dirname+'/public/first.html');
 });
 
 
